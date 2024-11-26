048c6db381747d4a8049b1e1db8f3d192f02c8a9
Milana Vorobyeva <mivorobyeva@ozon.ru>
2024-11-26 23:51:14 +0300

    [main] cors

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 	"svi_danie/internal/handlers"
@@ -62,132 +63,40 @@ func main() {
 
 	projService.PageService = pageService
 
-	// Регистрируем маршрут
-	http.HandleFunc("/add_user", userHandler.AddUser)
+	// Создаем маршрутизатор
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/add_proj", projHandler.CreateProj)
-	http.HandleFunc("/delete_proj", projHandler.DeleteProj)
-	http.HandleFunc("/get_proj", projHandler.GetProj)
-	http.HandleFunc("/get_all_proj", projHandler.GetAllProj)
-
-	http.HandleFunc("/add_page", pageHandler.CreatePage)
-	http.HandleFunc("/delete_page", pageHandler.DeletePage)
-	http.HandleFunc("/edit_page", pageHandler.EditPage)
-	http.HandleFunc("/get_page", pageHandler.GetPage)
-	http.HandleFunc("/get_all_pages", pageHandler.GetAllProjectPages)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// Регистрируем маршруты
+	mux.HandleFunc("/add_user", userHandler.AddUser)
+	mux.HandleFunc("/add_proj", projHandler.CreateProj)
+	mux.HandleFunc("/delete_proj", projHandler.DeleteProj)
+	mux.HandleFunc("/get_proj", projHandler.GetProj)
+	mux.HandleFunc("/get_all_proj", projHandler.GetAllProj)
+	mux.HandleFunc("/add_page", pageHandler.CreatePage)
+	mux.HandleFunc("/delete_page", pageHandler.DeletePage)
+	mux.HandleFunc("/edit_page", pageHandler.EditPage)
+	mux.HandleFunc("/get_page", pageHandler.GetPage)
+	mux.HandleFunc("/get_all_pages", pageHandler.GetAllProjectPages)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Добро пожаловать на сервер!") // Отправляет сообщение на корневом маршруте
 	})
 
+	// Настройка CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3001"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+	})
+
+	// Оборачиваем маршрутизатор в обработчик CORS
+	handler := c.Handler(mux)
+
 	// Запускаем сервер
 	port := "5003"
-	fmt.Printf("Сервер запущен на http://localhost:%s", port)
-	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	fmt.Printf("Сервер запущен на http://localhost:%s\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), handler)
 	if err != nil {
 		log.Println(err)
 	}
-
-	//pageRepo := &repositories.PageRepository{
-	//	Db: db,
-	//}
-
-	//userRepo := &repositories.UserRepository{
-	//	Db: db,
-	//}
-
-	// Пример создания нового пользователя
-	//newUser := models.User{
-	//	Id:       uuid.New(),
-	//	Login:    "new_user",
-	//	Password: "password123",
-	//}
-	//err = userRepo.Create(newUser)
-	//if err != nil {
-	//	fmt.Printf("create: %s\n", err)
-	//}
-	//
-	//// Пример чтения пользователя
-	//userID := newUser.Id
-	//user, err := userRepo.Read(userID)
-	//if err != nil {
-	//	fmt.Printf("read: %s\n", err)
-	//} else {
-	//	fmt.Printf("id: %s\nlogin: %s\n", user.Id, user.Login)
-	//}
-	//
-	//// Пример обновления пользователя
-	//updatedUser := models.User{
-	//	Id:       userID,
-	//	Login:    "updated_user",
-	//	Password: "new_password123",
-	//}
-	//err = userRepo.Update(updatedUser)
-	//if err != nil {
-	//	fmt.Printf("update: %s\n", err)
-	//}
-
-	// Пример удаления пользователя
-	//err = userRepo.Delete(uuid.MustParse("99af3993-2402-402e-bad8-c277c2e0485e"))
-	//if err != nil {
-	//	fmt.Printf("delete: %s\n", err)
-	//}
-
-	//// Пример создания новой страницы
-	//newPage := models.Page{
-	//	Id:      uuid.New(),
-	//	OwnerId: userID,
-	//	Title:   "New Page Title",
-	//	Data:    json.RawMessage(`{"key": "value"}`),
-	//}
-	//err = pageRepo.Create(newPage)
-	//if err != nil {
-	//	fmt.Printf("create: %s\n", err)
-	//}
-	//
-	//// Пример чтения страницы
-	//pageID := newPage.Id
-	//page, err := pageRepo.Read(pageID)
-	//if err != nil {
-	//	fmt.Printf("read: %s\n", err)
-	//} else {
-	//	fmt.Printf("id: %s\ntitle: %s\n", page.Id, page.Title)
-	//}
-	//
-	//// Пример обновления страницы
-	//updatedPage := models.Page{
-	//	Id:      newPage.Id,
-	//	OwnerId: newPage.OwnerId,
-	//	Title:   "Updated Page Title",
-	//	Data:    json.RawMessage(`{"key": "updated_value"}`),
-	//}
-	//err = pageRepo.Update(updatedPage)
-	//if err != nil {
-	//	fmt.Printf("update: %s", err)
-	//}
-	//
-	//// Пример удаления страницы
-	//err = pageRepo.Delete(newPage.Id)
-	//if err != nil {
-	//	fmt.Printf("update: %s", err)
-	//}
-
-	//// Пример выполнения запроса
-	//rows, err := db.Query("SELECT version();")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer rows.Close()
-	//
-	//for rows.Next() {
-	//	var version string
-	//	if err := rows.Scan(&version); err != nil {
-	//		log.Fatal(err)
-	//	}
-	//	fmt.Println("PostgreSQL version:", version)
-	//}
-	//
-	//if err := rows.Err(); err != nil {
-	//	log.Fatal(err)
-	//}
 }
